c215cfd1aabb623ca4d699669369cb41ad5513ef
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2026-06-30 15:56:36 +0530

    fixed-prismaAdpter-3006

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,15 +1,23 @@
 import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_KJT43WqMwafR@ep-frosty-boat-ah9igkh1-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = global as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({
+  globalForPrisma.prisma ??
+  new PrismaClient({
     adapter,
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
