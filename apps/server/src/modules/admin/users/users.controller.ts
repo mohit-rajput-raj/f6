de70f5d26837973ef67baae1d52f7f31e48681ea
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2026-09-16 21:19:15 +0530

    server-infrastructure-fix-2

--- apps/server/src/modules/admin/users/users.controller.ts
+++ apps/server/src/modules/admin/users/users.controller.ts
@@ -1,17 +1,16 @@
 import type { Request, Response } from "express";
-import { userService } from "./users.service";
+import { userService } from "./users.service.js";
 
-
-export class UsersController{
-    async getUsers(req:Request , res:Response){
-        const users = await userService.getAllusers();
-        res.json({success:true , data:users});
-    }
-     async getUserById(req:Request , res:Response){
-        const userId = req.params.id as string;
-        const user = await userService.getUserById(userId);
-        res.json({success:true , data:user});
-    }
+export class UsersController {
+  async getUsers(req: Request, res: Response) {
+    const users = await userService.getAllusers();
+    res.json({ success: true, data: users });
+  }
+  async getUserById(req: Request, res: Response) {
+    const userId = req.params.id as string;
+    const user = await userService.getUserById(userId);
+    res.json({ success: true, data: user });
+  }
 }
 
 export const usersController = new UsersController();
--- apps/server/src/modules/admin/users/users.routes.ts
+++ apps/server/src/modules/admin/users/users.routes.ts
@@ -1,7 +1,7 @@
-import {Router} from "express"
-import { asyncHandler } from "../../../middlewares/error-handler"
-import { usersController } from "./users.controller";
-const router:Router = Router();
-router.get("/users/" , asyncHandler(usersController.getUsers));
-router.get("/user/:id" , asyncHandler(usersController.getUserById));
-export default router
+import { Router } from "express";
+import { asyncHandler } from "../../../middlewares/error-handler.js";
+import { usersController } from "./users.controller.js";
+const router: Router = Router();
+router.get("/users/", asyncHandler(usersController.getUsers));
+router.get("/user/:id", asyncHandler(usersController.getUserById));
+export default router;
